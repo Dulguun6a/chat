922fdd98669a43e44c279ad0c4e862fb1f78c48e
Dulguun6a <80145304+Dulguun6a@users.noreply.github.com>
2024-02-19 04:54:12 +0000

    k

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ const names = [];
 window.onload = PrintNames()
 
 function PrintNames(){
-    let box = '';
-    if(document.getElementById("you").addEventListener(onclick)){
+    let box = ' ';
+    let box2 = ' ';
         for(let i = 0; i < names.length; i++){
             box += "You:" + ' ' + names[i] + '<br>';
         }
-    }
-    if(document.getElementById("tulga").addEventListener(onclick)){
-        for(let i = 0; i < names.length; i++){
-            box += "Tulga:" + ' ' + names[i] + '<br>';
-        }
-    }
     document.getElementById('list').innerHTML = box;
-
-}
+        for(let i = 0; i < names.length; i++){
+            box2 += "Dulguun:" + ' ' + names[i] + '<br>';
+        }
+    document.getElementById('list2').innerHTML = box2;
+    }
+function PrintNames2(){
+        let box = ' ';
+        let box2 = ' ';
+            for(let i = 0; i < names.length; i++){
+                box += "Orgil:" + ' ' + names[i] + '<br>';
+            }
+        document.getElementById('list').innerHTML = box;
+            for(let i = 0; i < names.length; i++){
+                box2 += "You:" + ' ' + names[i] + '<br>';
+            }
+        document.getElementById('list2').innerHTML = box2;
+        }
 function SaveName(){
     let newname = document.getElementById('newname').value;
     names.push(newname)
     PrintNames()
-    document.getElementById(newname).value = " ";
+    document.getElementById('newname').value = "";
+}
+function SaveName2(){
+    let newname2 = document.getElementById('newname2').value;
+    names.push(newname2)
+    PrintNames2()
+    document.getElementById('newname2').value = "";
 }
